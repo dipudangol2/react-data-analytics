91870fe2f952ba93d87f215d3215a99d19a0342e
dipudangol2 <dipudangol2@gmail.com>
2024-07-02 16:57:48 +0545

    Complete Cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,8 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Analytics from './components/Analytics'
-<<<<<<< HEAD
 import Cards from './components/Cards'
-=======
 import Newsletter from './components/Newsletter'
->>>>>>> 2306345d204bfadb420c76136f7c4f0996ecd207
-
 function App() {
 
   return (
@@ -15,11 +11,8 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
-<<<<<<< HEAD
-      <Cards />
-=======
       <Newsletter />
->>>>>>> 2306345d204bfadb420c76136f7c4f0996ecd207
+      <Cards />
     </>
   )
 }
